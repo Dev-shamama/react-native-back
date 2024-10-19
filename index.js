4f0be381663bec286fb708a460f3a91232ae9e26
Dev-shamama <148217035+Dev-shamama@users.noreply.github.com>
2024-10-19 22:25:26 +0500

    add one endpoint for test purpose

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
 const connectDB = require('./db/db');
 const Task = require('./model/Task');
 const User = require('./model/User');
-const {isAuthenticatedUser} = require('./middleware/auth');
+const { isAuthenticatedUser } = require('./middleware/auth');
 const app = express();
 require('dotenv').config();
 
@@ -23,7 +23,7 @@ connectDB();
 
 //** Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 //** Cloudinary Image Upload Configuration
 cloudinary.config({
@@ -35,8 +35,8 @@ cloudinary.config({
 //** APIs Router
 app.get('/api/v1/get', isAuthenticatedUser, async (req, res) => {
   try {
-    const data = await Task.find({userId: req.user._id});
-    res.json({success: true, data});
+    const data = await Task.find({ userId: req.user._id });
+    res.json({ success: true, data });
   } catch (error) {
     console.log(error);
   }
@@ -44,15 +44,15 @@ app.get('/api/v1/get', isAuthenticatedUser, async (req, res) => {
 
 app.post('/api/v1/save', isAuthenticatedUser, async (req, res) => {
   const user = req.user;
-  const exist = await Task.find({userId: req.user._id});
-  let payload = {...req.body, userId: user._id};
+  const exist = await Task.find({ userId: req.user._id });
+  let payload = { ...req.body, userId: user._id };
   if (exist.length === 0) {
     const data = Task(payload);
     await data.save();
-    return res.json({success: true, message: 'Data Save Successfully'});
+    return res.json({ success: true, message: 'Data Save Successfully' });
   } else {
-    await Task.updateOne({userId: req.user._id}, payload);
-    return res.json({success: true, message: 'Data Synced Successfully'});
+    await Task.updateOne({ userId: req.user._id }, payload);
+    return res.json({ success: true, message: 'Data Synced Successfully' });
   }
 });
 
@@ -64,11 +64,11 @@ app.post('/api/v1/save', isAuthenticatedUser, async (req, res) => {
 
 app.post('/api/v1/register', async (req, res) => {
   try {
-    const {name, email, password, gender} = req.body;
+    const { name, email, password, gender } = req.body;
 
-    const user = await User.find({email});
+    const user = await User.find({ email });
     if (user.length > 0) {
-      return res.json({success: false, message: 'Email is already exist'});
+      return res.json({ success: false, message: 'Email is already exist' });
     }
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
@@ -79,16 +79,16 @@ app.post('/api/v1/register', async (req, res) => {
       gender,
     });
     await data.save();
-    res.json({success: true, message: 'Account Create Successfully'});
+    res.json({ success: true, message: 'Account Create Successfully' });
   } catch (error) {
-    res.json({error});
+    res.json({ error });
   }
 });
 
 //** Login
 app.post('/api/v1/login', async (req, res) => {
   try {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
     if (!email || !password) {
       return res.json({
@@ -97,7 +97,7 @@ app.post('/api/v1/login', async (req, res) => {
       });
     }
 
-    const user = await User.findOne({email});
+    const user = await User.findOne({ email });
 
     if (!user) {
       return res.json({
@@ -114,9 +114,9 @@ app.post('/api/v1/login', async (req, res) => {
         message: 'Invalid email and password',
       });
     }
-    const payload = {id: user._id};
+    const payload = { id: user._id };
     const token = jwt.sign(payload, secret_key);
-    res.json({success: true, message: 'Login Successfully', token});
+    res.json({ success: true, message: 'Login Successfully', token });
   } catch (error) {
     console.log(error);
   }
@@ -124,7 +124,7 @@ app.post('/api/v1/login', async (req, res) => {
 
 app.get('/api/v1/me', isAuthenticatedUser, async (req, res) => {
   const data = await User.findById(req.user._id).select('-password');
-  res.json({success: true, data});
+  res.json({ success: true, data });
 });
 
 //** Profile Update
@@ -170,12 +170,19 @@ app.post(
 
       res
         .status(200)
-        .json({success: true, message: 'Profile Update Successfully'});
+        .json({ success: true, message: 'Profile Update Successfully' });
     } catch (error) {
       console.log(error);
     }
   },
 );
+
+
+app.get('/api/v1/test', async (req, res) => {
+  res.json({ success: true, data: "Welcome" });
+
+});
+
 
 //** Server Listen
 app.listen(port, () =>
